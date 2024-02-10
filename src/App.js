95729bf6c6f952c566ba5beeb312c8f528a61fe1
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2024-02-10 11:02:12 +0900

    stylesheet変更

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export const App = () => {
+  const [num, setNum] = useState(0);
+
+  const increment = () => {
+    setNum(num + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-white">
+      <h1 className="text-4xl text-center">Hello world!</h1>
+      <div className="text-center">counter</div>
+      <button onClick={() => { increment() }} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Increment
+      </button>
+      <p>{num}</p>
     </div>
   );
-}
-
-export default App;
+};
