e07f265f5aac45f30df0ef757f85ede548606304
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2024-02-12 10:15:08 +0900

    DaisyUIテスト

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
-
 export const Home = () => {
   return (
-    <div>
+    <div className="text-center">
       <h1>Home</h1>
+      <button className="btn btn-primary">Click me</button>
     </div>
   );
 }
