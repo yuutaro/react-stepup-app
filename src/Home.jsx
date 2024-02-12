3d7ff8005009e72ed1c398235fb1d6c3689a4b61
yuutaro-desktop <yuutaro.mikasa@gmail.com>
2024-02-12 10:55:43 +0900

    React入門5.44

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,25 @@
 export const Home = () => {
+  const clickHandler = () => {
+    alert("Hello, world!");
+  };
+
   return (
     <div className="text-center">
       <h1>Home</h1>
-      <button className="btn btn-primary">Click me</button>
+      <button className="btn btn-primary" onClick={clickHandler}>Click me</button>
+      <br />
+      <br />
+      <input type="text" className=" border border-gray-950 rounded-lg" placeholder="Type something"
+        onChange={(e) => { console.log(e.target.value) }}
+        onBlur={() => { console.log("onBlur発火") }}
+        onFocus={() => { console.log("onFocus発火") }}
+      />
+      <br />
+      <br />
+      <div className="w-full h-20 bg-cyan-200 hover:bg-cyan-300 active:bg-cyan-400 focus:bg-cyan-500"
+        onMouseEnter={() => { console.log("onMouseEnter発火") }}
+        onMouseLeave={() => { console.log("onMouseLeave発火") }}
+      >Hover me</div>
     </div>
   );
 }
